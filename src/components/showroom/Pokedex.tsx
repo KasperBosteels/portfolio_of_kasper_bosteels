598f6e56933922f4f65f3d1f5b40b8baf49038d2
Kasper B <kasperbosteels@hotmail.com>
2022-12-10 02:35:04 +0100

    pokedex styling

--- src/components/showroom/Pokedex.tsx
+++ src/components/showroom/Pokedex.tsx
@@ -1,3 +1,5 @@
+import { Box, Button, CircularProgress, TextField } from "@mui/material";
+import axios from "axios";
 import { useEffect, useState } from "react";
 import { RotatingTriangles } from "react-loader-spinner";
 interface IPokedex {
@@ -7,36 +9,52 @@ interface Result {
   name: string;
   url: string;
 }
-const Pokedex = ({ limit = 151 }: IPokedex) => {
+const Pokedex = ({ limit = 10 }: IPokedex) => {
   const [pokemon, setPokmen] = useState<Result[]>([]);
+  const [inputFilter,setFilter] = useState<string>("")
+  const [limitValue,setLimitValue] = useState<number>(limit)
   const [isloading, setIsloading] = useState<boolean>(true);
+  const fetchFunction = async () => {
+    setIsloading(true)
+    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=${limitValue < 0 ? 0 : limitValue}`).then((res)=>{
+    const resStringified = JSON.stringify(res.data.results)
+    setPokmen(JSON.parse(resStringified))
+    setIsloading(false)
+    })
+    
+  };
   useEffect(() => {
-    const fetchFunction = async () => {
-      let result = await fetch(
-        `https://pokeapi.co/api/v2/pokemon?limit=${limit}`
-      );
-      let json = await result.json();
-      setPokmen(json);
-      setIsloading(false);
-    };
     fetchFunction();
   }, []);
   return (
     <>
-      <div>
+
+      <Box>
+        <Box sx={{display:"flex", justifyContent:"center"}}>
+          <TextField
+            label="Filter by Name"
+            size="small"
+            onChange={(e)=>setFilter(e.target.value)}/>
+          </Box>
+          <Box sx={{justifyContent:"center", display:"grid"}}>  
         {isloading ? (
-          <RotatingTriangles
-            visible={true}
-            height="80"
-            width="80"
-            ariaLabel="rotating-triangels-loading"
-            wrapperStyle={{}}
-            wrapperClass="rotating-triangels-wrapper"
-          />
-        ) : (
-          pokemon.map((poke) => <p>{poke.name}</p>)
-        )}
-      </div>
+          <CircularProgress sx={{margin:"2rem"}}/>
+        ) : (<>{pokemon.length > 0 ? (pokemon.filter((p)=>p.name.startsWith(inputFilter.toLowerCase())).map((p,i)=>(<p key={i}>{p.name.normalize()}</p>))) :(<></>)}</>)}
+      
+     
+      </Box>
+      <Box sx={{display:"flex", justifyContent:"center", marginTop:"1rem"}}>
+      <TextField
+      type="number"
+      label="Limit pokemon"
+      size="small"
+      InputLabelProps={{shrink:true}}
+      sx={{borderRadius:0}}
+      onChange={(e)=>setLimitValue(parseInt(e.target.value ))}
+      />
+      <Button variant="contained" sx={{borderRadius:0}} onClick={()=>fetchFunction()}>Set Limit</Button>
+      </Box>
+      </Box>
     </>
   );
 };
