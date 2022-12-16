8a41d1adf5f49c3d758514e154e64062a325d175
Kasper B <kasperbosteels@hotmail.com>
2022-12-16 21:48:51 +0100

    limited data in component interface

--- src/pages/showRoom.tsx
+++ src/pages/showRoom.tsx
@@ -11,46 +11,37 @@ import { useParams } from "react-router-dom";
 import ComponentDoesNotExist from "../components/ComponentDoesNotExist";
 interface components {
     name:string,
-    description:string,
     component:JSX.Element
     }
 
 const componenten:components[] = [
     {
         name:"Shopping list",
-        description:"An empty shopping list with an input field that allows you to add to it.",
         component:<ShoppingList/>
     },
     {
         name:"Filtering and sorting",
-        description:"A component with a list that you can filter & sort, wich will update live.",
         component:<Filtering/>
     },
     {
         name:"Slots",
-        description:"A slot machine that simulates one from a casino with money.",
         component:<Slotmachine slots={3}/>
     },
     {
         name:"Counter",
-        description:"This is a list with multiple counters triggered by buttons.",
         component:<Counter/>
     },{
         name:"Interval",
-        description:"This component contains various time triggered displays",
         component:<TimeContainer/>
     },{
         name:"Pokedex",
-        description:"This component will call the pokeApi and simulate a pokedex.",
         component:<Pokedex limit={5}/>
     },
     {
         name:"DadJoke-local storage",
-        description:"A component that calls an api, and displays its answer.",
         component:<DadJoke/>
     },{
         name:"Todo list",
-        description:"A component that simulates a todo list with communication between parent and child",
         component:<TodoComponent/>
     }
 ];
