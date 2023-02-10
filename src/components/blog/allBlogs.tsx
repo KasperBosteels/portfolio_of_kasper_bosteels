625f48e9731eecbf3cce312052d22014ef774791
Kasper B <kasperbosteels@hotmail.com>
2023-02-10 17:09:56 +0100

    added rudimentary filter functionality

--- src/components/blog/allBlogs.tsx
+++ src/components/blog/allBlogs.tsx
@@ -1,7 +1,9 @@
 import BlogArticle from "./blogArticle";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Box from "@mui/material/Box";
 import  CircularProgress  from "@mui/material/CircularProgress";
+import { TextField } from "@mui/material";
+import { Filter } from "@mui/icons-material";
 export interface post {
     date:string,
     text:string,
@@ -11,10 +13,32 @@ export interface post {
 const AllBlogs = ()=>{
 const [posts,setPosts] = useState<post[]>(require("./blogs/blogs.json").Posts.sort((a:post,b:post)=> new Date(a.date) < new Date(b.date)?+1 :0) ?? []);
 const [FilteredPOsts, setFilteredPosts] = useState<post[]>(posts);
+const [FilterText,setFilterText] = useState<string>("");
+
+const EditText =(input:string)=>{
+ setFilterText(input??"");
+}
+useEffect(() => {
+  
+    setFilteredPosts(posts.filter(x=>x.title?.toLowerCase().includes(FilterText.toLowerCase())))
+
+}, [FilterText])
 
 return (
 
-    <Box>
+    <Box sx={{marginBottom:"1rem"}}>
+    <Box
+        sx={{
+          display:"flex",
+          alignContent:"center",
+          justifyContent:"center",  
+          marginTop:"2rem",
+          padding:"4rem",
+        }}
+        >
+        <TextField variant="outlined"label="Filter By title"
+            onChange={(e)=>EditText(e.target.value)}
+            />
     <Box sx={{
         display:"flex", 
         flexDirection:"column", 
@@ -25,9 +49,10 @@ return (
         {posts.length>0 ?  
             (
                 <>
-                {posts.map((p,i)=><BlogArticle key={"post-"+i} post={p} maxWidth="70%" button={false} tags={p.tags}/>)}
+                {FilteredPOsts.map((p,i)=><BlogArticle key={"post-"+i} post={p} maxWidth="70%" button={false} tags={p.tags}/>)}
                 </>
                 ) : <CircularProgress  sx={{marginTop:"15%"}}/>}
+    </Box>
     </Box>
     </Box>
 )
