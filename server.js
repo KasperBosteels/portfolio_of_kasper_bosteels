4798f83f40903bc3c9114793b02a9195c7fe5f1f
Kasper B <kasperbosteels@hotmail.com>
2023-08-21 18:31:33 +0200

    removed uneeded stuff

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 const express = require("express")
-const csp = require('express-csp-header')
 const bodyParser = require("body-parser")
 const path = require("path")
 const app = express();
 app.use(express.static(path.join(__dirname,'build')))
-app.use(csp({
-    policies: {
-        'default-src': [csp.NONE],
-        'img-src': [csp.SELF],
-    }
-}));
 app.get("/",function (req,res){
     res.sendFile(path.join(__dirname,'build','index.html'))
 })
